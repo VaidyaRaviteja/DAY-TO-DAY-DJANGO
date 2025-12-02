e0a36dacf0ac967df1a5544107b87304f3d126de
Raviteja <vydyatej@mail.com>
2025-12-02 11:17:28 +0530

    Day 1

---
+++ first_project/first_app/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+# def home(request):
+#     return HttpResponse("""
+#     <!DOCTYPE html>
+#     <html lang="en">
+#     <head>
+#         <meta charset="utf-8">
+#         <title>It worked!</title>
+#         <style>
+#             body {
+#                 background-color: #0d1117;
+#                 color: white;
+#                 font-family: sans-serif;
+#                 text-align: center;
+#                 padding-top: 50px;
+#             }
+#             h1 {
+#                 font-size: 3em;
+#             }
+#             img {
+#                 width: 300px;
+#                 margin-top: 30px;
+#             }
+#         </style>
+#     </head>
+#     <body>
+#         <h1>It worked! 🎉</h1>
+#         <p>Your Django installation is working perfectly.</p>
+#         <img src="https://upload.wikimedia.org/wikipedia/commons/7/75/Django_logo.svg" alt="Django Rocket">
+#     </body>
+#     </html>
+#     """)
+
+# def test(request):
+#     return HttpResponse("Hello world this django series")
+
+
+
+# def test(self):
+#     return HttpResponse("Hello world this django series")
